c64ee7a884009e7a34c00b087a4dd52162e50f58
amadoukane96 <amadoukane96@gmail.com>
2018-09-11 09:29:19 -0400

    fix duplicate ids

--- app.py
+++ app.py
@@ -758,7 +758,7 @@ def modal(pair):
                         ),
                         html.Span(
                             pair,
-                            id="modal_pair",
+                            id="modal"+pair,
                             style={"marginBottom": "10", "color": "#45df7e"},
                         ),
 
